6bf4a8652fbd2ffb761f868b1d44567cf676bd4f
Sreya Vadlamudi <vadlamudi@chapman.edu>
2020-11-17 19:26:31 -0800

    updates regex

--- username.sh
+++ username.sh
@@ -1,12 +1,16 @@
 #! /bin/bash
 # username.sh
 # Sreya Vadlamudi
-echo "Enter a five-digit ZIP code: "
-read ZIP
-while echo "$ZIP" | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+echo "Enter a username: "
+echo "Use lower case characters, digits, and underscore"
+echo "It must start with a lower case character"
+echo "It must contain at least 3 but no more than 12 characters"
+
+read USERNAME
+while echo "$USERNAME" | egrep -v "^[a-z]{1}[a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid ZIP code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
-	read ZIP
+	echo "You must enter a valid username!"
+	echo "Enter a username: "
+	read USERNAME
 done
 echo "Thank you"
